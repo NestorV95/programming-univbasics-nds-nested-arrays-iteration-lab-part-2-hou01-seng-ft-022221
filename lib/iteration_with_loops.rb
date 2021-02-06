b133ea71a21fd7681dfba40b78abb43ff827840a
Nestor Venegas <nestor.venegas95@gmail.com>
2021-02-06 23:04:46 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,13 +6,15 @@ row_index = 0
 
    while row_index < src.count do 
     element_index = 0 
-  
-       while element_index < src[row_index].count do
-         if src.min([row_index][element_index])
-       new_array << src[row_index][element_index]
+    min_value = 50
+    
+       while element_index > src[row_index].count do
+         if src.min[row_index][element_index] < min_value
+         min_value = src[row_index][element_index]
          end
        element_index +=1
        end
+    new_array << min_value
     row_index +=1
     end
   new_array
